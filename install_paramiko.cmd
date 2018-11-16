daf51b865679eb0be7c9ac13db45ea4c42e7f0b6
brandonrumer <44004200+brandonrumer@users.noreply.github.com>
2018-11-15 20:15:49 -0500

    Add files via upload

---
+++ install_paramiko.cmd
@@ -0,0 +1,9 @@
+pip install .\pycparser-master\.
+pip install .\cffi\cffi-1.11.5-cp36-cp36m-win_amd64.whl
+pip install .\bcrypt\bcrypt-3.1.4-cp36-cp36m-win_amd64.whl
+pip install .\asn1crypto-master\.
+pip install .\cryptography\cryptography-2.3.1-cp36-cp36m-win_amd64.whl
+pip install .\pynacl\PyNaCl-1.2.1-cp36-cp36m-win_amd64.whl
+pip install .\paramiko-master\.
+pause
+
---
+++ install_requests.cmd
@@ -0,0 +1,9 @@
+pip install .\urllib3-1.23\.
+pip install .\idna-2.7\.
+pip install .\chardet-3.0.4\.
+pip install .\certifi-2018.8.13\.
+pip install .\requests-2.19.1\.
+pip install .\six-1.11.0\.
+pip install .\orionsdk-python-master\.
+pause
+
